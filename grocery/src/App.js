ed2eab330ede57eb31ff6a13510437b94f3e20d2
Aseem Ratha <aseemratha@gmail.com>
2023-02-23 22:45:43 -0500

    Initial commit

---
+++ grocery/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const texts = [
+  "First text",
+  "Second text",
+  "Third text",
+  "Fourth text"
+];
+
+function App() {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const handleNextText = () => {
+    const newIndex = currentTextIndex + 1 < texts.length ? currentTextIndex + 1 : 0;
+    setCurrentTextIndex(newIndex);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">{texts[currentTextIndex]}</h1>
+      <button className="button" onClick={handleNextText}>Next Text</button>
+    </div>
+  );
+}
+
+export default App;

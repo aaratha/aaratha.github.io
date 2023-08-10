2c4a07b6a5272ed39aeed1e1b541f9d9670ed7c9
Aseem Ratha <aseemratha@gmail.com>
2023-08-10 12:05:45 -0400

    tweaks

--- src/app/components/websites.tsx
+++ src/app/components/websites.tsx
@@ -12,7 +12,7 @@ import img3 from '../images/site3.png'
  */
 export default function Websites() {
     return (
-        <div className="h-screen md:w-[35rem] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-5">
+        <div className="h-screen md:w-[50vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-5">
             <div className='flex flex-col'>
                 <a href='/'><Image src={img1} alt='site1' className='rounded-lg mt-5 mb-5 border-2 border-amber-900 hover:scale-95 transition-all'></Image></a>
                 <a href='https://www.wec.education/'><Image src={img3} alt='site2' className='rounded-lg mb-5 border-2 border-amber-900 hover:scale-95 transition-all'></Image></a>

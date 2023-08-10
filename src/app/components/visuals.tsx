f29daca393181fce21fd15d1f8b5d21996986c54
Aseem Ratha <aseemratha@gmail.com>
2023-08-10 12:18:24 -0400

    fasfaf

--- src/app/components/visuals.tsx
+++ src/app/components/visuals.tsx
@@ -2,21 +2,30 @@
 
 import Image from 'next/image'
 
-import img1 from '../images/img1.png'
-import img2 from '../images/img2.png'
-import img3 from '../images/img3.jpg'
-import img4 from '../images/img4.png'
-import img5 from '../images/img5.jpg'
+const images = [
+    require('../images/img1.png'),
+    require('../images/img2.png'),
+    require('../images/img3.jpg'),
+    require('../images/img4.png'),
+    require('../images/img5.jpg'),
+    require('../images/img6.jpeg'),
+    require('../images/img7.jpeg'),
+    require('../images/img8.jpeg'),
+    require('../images/img9.jpeg'),
+    require('../images/img10.jpeg'),
+    require('../images/img11.jpg'),
+    require('../images/img12.jpg'),
+    require('../images/img13.jpeg'),
+    require('../images/img14.jpeg'),
+]
 
 export default function Visuals() {
     return (
         <div className="h-screen md:w-[50vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-5">
             <div className='flex flex-col'>
-                <Image src={img1} alt='img1' className='rounded-lg mt-5 mb-5'></Image>
-                <Image src={img3} alt='img2' className='rounded-lg mb-5'></Image>
-                <Image src={img2} alt='img3' className='rounded-lg mb-5 w-full'></Image>
-                <Image src={img4} alt='img4' className='rounded-lg mb-5'></Image>
-                <Image src={img5} alt='img5' className='rounded-lg mb-5'></Image>
+            {images.map((image, index) => (
+            <Image key={index} src={image.default} alt={`img${index + 1}`} className='rounded-lg mt-5 mb-5 w-full' />
+            ))}
             </div>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,28 @@ import { useState, useEffect } from 'react'
 import svg from 'src/app/images/home.svg'
 
 export default function Home() {
-  const [showSubOptions, setShowSubOptions] = useState(false);
+    const [showSubOptions, setShowSubOptions] = useState(false);
 
-  return (
-      <div className="bg-orange-200 min-h-screen flex flex-col md:flex-row">
-          <div className="m-20 mb-0 mr-10 flex flex-col font-serif text-orange-900 z-10">
-              <h1 className='text-4xl font-bold '>Aseem Ratha</h1>
-              <div className='text-xl flex flex-col'>
-                  <a href='/' className='hover:pl-6 pl-3 ml-2 mt-2 transition-all border-l border-orange-900 font-bold'><button>Home</button></a>
-                  <a href='/projects' className='hover:pl-6 pl-3 ml-2 transition-all border-l border-orange-900' onMouseEnter={() => setShowSubOptions(true)} onMouseLeave={() => setShowSubOptions(false)}><button>Projects</button></a>
-                  <div className='overflow-hidden ml-2'>
-                      <div className={`flex flex-col text-lg  sub-options border-l border-orange-900  ${showSubOptions ? 'show' : ''}`}>
-                          <a href='/videos' className='hover:translate-x-5  transition-all translate-x-3 ml-2'><button>• Videos</button></a>
-                          <a href='/visuals' className='hover:translate-x-5  transition-all translate-x-3 ml-2'><button>• Visuals</button></a>
-                          <a href='/websites' className='hover:translate-x-5  transition-all translate-x-3 ml-2'><button>• Websites</button></a>
-                          <a href='/contact' className='hover:pl-6 pl-3  transition-all text-xl'><button>Contact</button></a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div className='ml-3 md:m-auto'>
-            <Image src={svg} width={600} alt=''></Image>
-          </div>
-      </div>
-  )
+    return (
+        <div className="bg-orange-200 min-h-screen flex flex-col md:flex-row">
+            <div className="m-20 mb-0 mr-10 flex flex-col font-serif text-orange-900 z-10">
+                <h1 className='text-4xl font-bold '>Aseem Ratha</h1>
+                <div className='text-xl flex flex-col'>
+                    <a href='/' className='hover:pl-6 pl-3 ml-2 mt-2 transition-all border-l border-orange-900 font-bold'><button>Home</button></a>
+                    <a href='/projects' className='hover:pl-6 pl-3 ml-2 transition-all border-l border-orange-900' onMouseEnter={() => setShowSubOptions(true)} onMouseLeave={() => setShowSubOptions(false)}><button>Projects</button></a>
+                    <div className='overflow-hidden ml-2'>
+                        <div className={`flex flex-col text-lg  sub-options border-l border-orange-900  ${showSubOptions ? 'show' : ''}`}>
+                            <a href='/videos' className='hover:translate-x-5  transition-all translate-x-3 ml-2'><button>• Videos</button></a>
+                            <a href='/visuals' className='hover:translate-x-5  transition-all translate-x-3 ml-2'><button>• Visuals</button></a>
+                            <a href='/websites' className='hover:translate-x-5  transition-all translate-x-3 ml-2'><button>• Websites</button></a>
+                            <a href='/contact' className='hover:pl-6 pl-3  transition-all text-xl'><button>Contact</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='ml-3 md:m-auto'>
+                <Image src={svg} width={720} alt=''></Image>
+            </div>
+        </div>
+    )
 }

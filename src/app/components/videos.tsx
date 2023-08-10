39818641a6b82e13662e65203c31c1e76fc4af04
Aseem Ratha <aseemratha@gmail.com>
2023-08-10 12:39:45 -0400

    fgaga

--- src/app/components/videos.tsx
+++ src/app/components/videos.tsx
@@ -17,7 +17,7 @@ export default function Videos() {
     return (
         <div className=" h-full w-full flex flex-col pt-10 pb-0 pr-10 md:p-20 md:pl-0">
             <iframe className="w-full h-full rounded-lg min-h-[42vh]" src={videos[i]} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            <div className='flex flex-row items-center mt-1 md:mt-0 overflow-scroll overflow-y-hidden scrollbar-track-orange-200 scrollbar-thumb-amber-900 scrollbar-thin pb-2 pt-2'>
+            <div className='flex flex-row items-center mt-1 md:mt-0 overflow-scroll overflow-y-hidden scrollbar-track-orange-200 scrollbar-thumb-amber-900 scrollbar-thin pb-2 md:pt-2'>
                 <button onClick={() => setI(0)} className='p-1 pl-0 hover:scale-95 transition-all'><Image src={img1} alt='knox' className='rounded-lg min-w-[7rem]'></Image></button>
                 <button onClick={() => setI(1)} className='p-1 pl-0 hover:scale-95 transition-all min-w-[7rem]'><Image src={img2} alt='dreary' className='rounded-lg'></Image></button>
                 <button onClick={() => setI(2)} className='p-1 pl-0 hover:scale-95 transition-all min-w-[7rem]'><Image src={img3} alt='2814' className='rounded-lg'></Image></button>

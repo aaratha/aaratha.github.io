b0158396e0a522516a27d5a992312023b04efd7b
Aseem Ratha <aseemratha@gmail.com>
2024-01-07 17:05:43 -0500

    idk most recent

--- src/app/components/videos.tsx
+++ src/app/components/videos.tsx
@@ -12,7 +12,8 @@ export default function Videos() {
   return (
     <div className="md:border-l-2 md:pl-5 md:border-r-2 border-amber-900 h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-3 pt-8 md:pt-0">
       {videos.map((video, index) => (
-        <iframe key={index} src={video} alt={`video${index + 1}`} className='rounded-lg mt-5 mb-5 w-full h-full min-h-[42vh]' />
+        <iframe key={index} src={video} className='rounded-lg mt-5 w-full h-full min-h-[42vh]' />
+
       ))}
     </div>
   )
--- src/app/components/visuals.tsx
+++ src/app/components/visuals.tsx
@@ -3,31 +3,31 @@
 import Image from 'next/image'
 
 const images = [
-    require('../images/img1.png'),
-    require('../images/img2.png'),
-    require('../images/img3.jpg'),
-    require('../images/img4.png'),
-    require('../images/img5.jpg'),
-    require('../images/img6.jpeg'),
-    require('../images/img7.jpeg'),
-    require('../images/img8.jpeg'),
-    require('../images/img9.jpeg'),
-    require('../images/img10.jpeg'),
-    require('../images/img11.jpg'),
-    require('../images/img12.jpg'),
-    require('../images/img13.jpeg'),
-    require('../images/img14.jpeg'),
+  require('../images/img1.png'),
+  require('../images/img2.png'),
+  require('../images/img3.jpg'),
+  require('../images/img4.png'),
+  require('../images/img5.jpg'),
+  require('../images/img6.jpeg'),
+  require('../images/img7.jpeg'),
+  require('../images/img8.jpeg'),
+  require('../images/img9.jpeg'),
+  require('../images/img10.jpeg'),
+  require('../images/img11.jpg'),
+  require('../images/img12.jpg'),
+  require('../images/img13.jpeg'),
+  require('../images/img14.jpeg'),
 ]
 
 export default function Visuals() {
-    return (
-        <div className="md:border-l-2 md:pl-5 md:border-r-2 border-amber-900 h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-3 pt-8 md:pt-0">
-            <div className='flex flex-col'>
-            {images.map((image, index) => (
-            <Image key={index} src={image.default} alt={`img${index + 1}`} className='rounded-lg mt-5 mb-5 w-full' />
-            ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="md:border-l-2 md:pl-5 md:border-r-2 border-amber-900 h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-3 pt-8 md:pt-0">
+      <div className='flex flex-col'>
+        {images.map((image, index) => (
+          <Image key={index} src={image.default} alt={`img${index + 1}`} className='rounded-lg mt-5 w-full' />
+        ))}
+      </div>
+    </div>
+  )
 }
 

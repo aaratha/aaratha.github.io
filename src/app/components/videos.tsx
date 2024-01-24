bfe1033007dabdbd38ec8a9ee151a9ff3a0dffb7
Aseem Ratha <aseemratha@gmail.com>
2024-01-24 12:19:05 -0500

    added archive1 and vicrecords, increase padding on rails

--- src/app/components/videos.tsx
+++ src/app/components/videos.tsx
@@ -7,10 +7,10 @@ import { useState, useEffect } from 'react'
 export default function Videos() {
   const [i, setI] = useState(0);
 
-  let videos = ['https://www.youtube.com/embed/9eHqGEshB2A', 'https://www.youtube.com/embed/TBMEBSfnJbQ', 'https://www.youtube.com/embed/TybreaCetEA', 'https://www.youtube.com/embed/OcpRc_LU-e0', 'https://www.youtube.com/embed/buUa9jiD9os', 'https://www.youtube.com/embed/GSU93sFdRls']
+  let videos = ['https://www.youtube.com/embed/tkQt6GoE36o','https://www.youtube.com/embed/9eHqGEshB2A', 'https://www.youtube.com/embed/TBMEBSfnJbQ', 'https://www.youtube.com/embed/TybreaCetEA', 'https://www.youtube.com/embed/OcpRc_LU-e0', 'https://www.youtube.com/embed/buUa9jiD9os', 'https://www.youtube.com/embed/GSU93sFdRls']
 
   return (
-    <div className="md:border-l-2 md:pl-5 md:border-r-2 border-amber-900 h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-3 pt-8 md:pt-0">
+    <div className="md:border-l-2 md:pl-10 md:border-r-2 border-amber-900 h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-10 pt-8 md:pt-0 ">
       {videos.map((video, index) => (
         <iframe key={index} src={video} className='rounded-lg mt-5 w-full h-full min-h-[42vh]' />
 

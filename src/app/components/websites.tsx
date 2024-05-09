6bd7d25315e59b1f841434b4dcb80e12adf27f29
Aseem Ratha <aseemratha@gmail.com>
2024-05-09 03:24:27 +0000

    removed list shadow on mobile

--- src/app/components/websites.tsx
+++ src/app/components/websites.tsx
@@ -13,7 +13,7 @@ import img4 from '../images/vic-records.png'
  */
 export default function Websites() {
     return (
-        <div className="list-shadow md:border-l-2 md:pl-10 md:border-r-2 border-fg h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-10 pt-8 md:pt-0">
+        <div className="sm:list-shadow md:border-l-2 md:pl-10 md:border-r-2 border-fg h-screen md:w-[55vw] flex flex-col md:overflow-scroll md:overflow-x-hidden scrollbar-thin scrollbar-track-orange-200 scrollbar-thumb-amber-900 pr-8 md:pr-10 pt-8 md:pt-0">
             <div className='flex flex-col'>
                 <a href='https://vicrecords.netlify.app/'><Image src={img4} alt='vic-records' className='item-shadow rounded-lg mt-5 mb-5 border-2 border-amber-900 hover:scale-95 transition-all'></Image></a>
                 <a href='/'><Image src={img1} alt='site1' className='item-shadow rounded-lg mt-5 mb-5 border-2 border-amber-900 hover:scale-95 transition-all'></Image></a>
